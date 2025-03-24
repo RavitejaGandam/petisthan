618ed7c1db02e6040d024725e227b6c376337b66
Raviteja Gandam <gandamraviteja@gmail.com>
2025-03-24 11:32:37 +0530

    First commit

---
+++ src/main/java/com/ravi/petisthan/PetisthanApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.petisthan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetisthanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetisthanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravi/petisthan/PetisthanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.petisthan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetisthanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
